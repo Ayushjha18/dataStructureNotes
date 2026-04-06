dbd6a1bb9c5d0de4a109b366c6f00e9b437c2d95
Ayushjha18 <ayushjha5060@gmail.com>
2026-04-06 17:25:33 +0530

    queue

---
+++ src/Queues.java
@@ -0,0 +1,55 @@
+public class Queues {
+
+    private int front;
+    private int rear;
+    private int size;
+    private int[] queue = new int[10];
+
+
+    public void enqueue(int data) {
+
+        if (!isFull()) {
+            queue[rear] = data;
+            rear = (rear + 1) % 10;
+            size++;
+        } else {
+            System.out.println("Queue is full");
+        }
+
+    }
+
+
+    public int dequeue() {
+
+        if (!isEmpty()) {
+            throw new RuntimeException("Queue is empty");
+        }
+        int temp = queue[front];
+        front = (front + 1) % 10;
+        front++;
+        size--;
+
+        return temp;
+    }
+
+    public int peek() {
+        if (!isEmpty())
+            throw new RuntimeException("Queue is empty");
+        return queue[front];
+    }
+
+    public void showQueue() {
+        for (int i = front; i < size; i++) {
+            System.out.print(queue[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == queue.length;
+    }
+}
